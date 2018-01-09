8676c489b01f520321c7b12ae0be159881a10457
donfabio <35248463+donfabio@users.noreply.github.com>
2018-01-09 06:25:29 +0200

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonCircle = new System.Windows.Forms.Button();
+            this.buttonRectangle = new System.Windows.Forms.Button();
+            this.buttonEllipse = new System.Windows.Forms.Button();
+            this.buttonPlus = new System.Windows.Forms.Button();
+            this.buttonMinus = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.buttonColor = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBoxSize = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(683, 563);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            // 
+            // buttonCircle
+            // 
+            this.buttonCircle.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCircle.Location = new System.Drawing.Point(744, 13);
+            this.buttonCircle.Name = "buttonCircle";
+            this.buttonCircle.Size = new System.Drawing.Size(99, 38);
+            this.buttonCircle.TabIndex = 1;
+            this.buttonCircle.Text = "Circle";
+            this.buttonCircle.UseVisualStyleBackColor = true;
+            this.buttonCircle.Click += new System.EventHandler(this.buttonCircle_Click);
+            // 
+            // buttonRectangle
+            // 
+            this.buttonRectangle.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonRectangle.Location = new System.Drawing.Point(723, 57);
+            this.buttonRectangle.Name = "buttonRectangle";
+            this.buttonRectangle.Size = new System.Drawing.Size(139, 38);
+            this.buttonRectangle.TabIndex = 2;
+            this.buttonRectangle.Text = "Rectangle";
+            this.buttonRectangle.UseVisualStyleBackColor = true;
+            this.buttonRectangle.Click += new System.EventHandler(this.buttonRectangle_Click);
+            // 
+            // buttonEllipse
+            // 
+            this.buttonEllipse.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonEllipse.Location = new System.Drawing.Point(744, 101);
+            this.buttonEllipse.Name = "buttonEllipse";
+            this.buttonEllipse.Size = new System.Drawing.Size(99, 38);
+            this.buttonEllipse.TabIndex = 3;
+            this.buttonEllipse.Text = "Ellipse";
+            this.buttonEllipse.UseVisualStyleBackColor = true;
+            this.buttonEllipse.Click += new System.EventHandler(this.buttonEllipse_Click);
+            // 
+            // buttonPlus
+            // 
+            this.buttonPlus.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonPlus.Location = new System.Drawing.Point(845, 185);
+            this.buttonPlus.Name = "buttonPlus";
+            this.buttonPlus.Size = new System.Drawing.Size(38, 41);
+            this.buttonPlus.TabIndex = 5;
+            this.buttonPlus.Text = "+";
+            this.buttonPlus.UseVisualStyleBackColor = true;
+            this.buttonPlus.Click += new System.EventHandler(this.buttonPlus_Click);
+            // 
+            // buttonMinus
+            // 
+            this.buttonMinus.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonMinus.Location = new System.Drawing.Point(707, 185);
+            this.buttonMinus.Name = "buttonMinus";
+            this.buttonMinus.Size = new System.Drawing.Size(39, 41);
+            this.buttonMinus.TabIndex = 6;
+            this.buttonMinus.Text = "-";
+            this.buttonMinus.UseVisualStyleBackColor = true;
+            this.buttonMinus.Click += new System.EventHandler(this.buttonMinus_Click);
+            // 
+            // buttonColor
+            // 
+            this.buttonColor.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonColor.Location = new System.Drawing.Point(744, 293);
+            this.buttonColor.Name = "buttonColor";
+            this.buttonColor.Size = new System.Drawing.Size(99, 37);
+            this.buttonColor.TabIndex = 7;
+            this.buttonColor.Text = "Color Picker";
+            this.buttonColor.UseVisualStyleBackColor = true;
+            this.buttonColor.Click += new System.EventHandler(this.buttonColor_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(761, 347);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(69, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBoxSize
+            // 
+            this.textBoxSize.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxSize.Location = new System.Drawing.Point(764, 192);
+            this.textBoxSize.Name = "textBoxSize";
+            this.textBoxSize.Size = new System.Drawing.Size(63, 26);
+            this.textBoxSize.TabIndex = 13;
+            this.textBoxSize.TextChanged += new System.EventHandler(this.textBoxSize_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(897, 588);
+            this.Controls.Add(this.textBoxSize);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonColor);
+            this.Controls.Add(this.buttonMinus);
+            this.Controls.Add(this.buttonPlus);
+            this.Controls.Add(this.buttonEllipse);
+            this.Controls.Add(this.buttonRectangle);
+            this.Controls.Add(this.buttonCircle);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonCircle;
+        private System.Windows.Forms.Button buttonRectangle;
+        private System.Windows.Forms.Button buttonEllipse;
+        private System.Windows.Forms.Button buttonPlus;
+        private System.Windows.Forms.Button buttonMinus;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button buttonColor;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBoxSize;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+   
+
+    public partial class Form1 : Form
+    {
+
+        public int size = 100;
+        public int posX, posY;
+        public List<Shape> shapes = new List<Shape>();
+        public int activeShape = -1;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            textBox1.BackColor = colorDialog1.Color;
+            textBoxSize.Text = size.ToString();
+        }
+
+        
+
+        private void buttonCircle_Click(object sender, EventArgs e)
+        {
+
+            shapes.Add(new Shape(pictureBox1.Width / 2 - size / 2, pictureBox1.Height / 2 - size / 2,size,colorDialog1.Color,'c'));
+            activeShape = shapes.Count - 1;
+            drawShape();
+        }
+
+        private void buttonRectangle_Click(object sender, EventArgs e)
+        {
+            shapes.Add(new Shape(pictureBox1.Width / 2 - size / 2, pictureBox1.Height / 2 - size / 2, size, colorDialog1.Color, 'r'));
+            activeShape = shapes.Count - 1;
+            drawShape();
+        }
+
+        private void buttonEllipse_Click(object sender, EventArgs e)
+        {
+            shapes.Add(new Shape(pictureBox1.Width / 2 - size / 2, pictureBox1.Height / 2 - size / 2, size, colorDialog1.Color, 'e'));
+            activeShape = shapes.Count - 1;
+            drawShape();
+            
+        }
+
+        private void buttonPlus_Click(object sender, EventArgs e)
+        {
+            size += 25;
+            textBoxSize.Text = size.ToString();
+            if (activeShape != -1)
+            {
+                shapes[activeShape].SetSize(size);
+            }
+            drawShape();
+        }
+
+        private void buttonMinus_Click(object sender, EventArgs e)
+        {
+            if (size != 25)
+            {
+                size -= 25;
+                textBoxSize.Text = size.ToString();
+                if (activeShape != -1)
+                {
+                    shapes[activeShape].SetSize(size);
+                }
+                drawShape();
+            }
+        }
+
+        private void drawShape()
+        {
+            Graphics g = Graphics.FromImage(pictureBox1.Image);
+            g.FillRectangle(new SolidBrush(Color.White), 0, 0, pictureBox1.Width, pictureBox1.Height);
+            foreach(Shape s in shapes)
+            {
+                if (s.GetType() == 'c')
+                {
+                    g.FillEllipse(s.GetBrush(), s.GetX(), s.GetY(), s.GetSize(), s.GetSize());
+                }else if(s.GetType() == 'r')
+                {
+                    g.FillRectangle(s.GetBrush(), s.GetX(), s.GetY(), s.GetSize(), s.GetSize());
+                }
+                else
+                {
+                    g.FillEllipse(s.GetBrush(), s.GetX(), s.GetY(), s.GetSize()*2, s.GetSize());
+                }
+            }
+            
+            pictureBox1.Refresh();
+        }
+
+        
+
+        private void buttonColor_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            textBox1.BackColor = colorDialog1.Color;
+            shapes[activeShape].SetBrush(colorDialog1.Color);
+            drawShape();
+        }
+
+       
+        private void textBoxSize_TextChanged(object sender, EventArgs e)
+        {
+            size = Int32.Parse(textBoxSize.Text);
+            if (activeShape != -1)
+            {
+                shapes[activeShape].SetSize(size);
+            }
+            drawShape();
+        }
+
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            int i = 0;
+            bool change = false;
+            foreach(Shape s in shapes)
+            {
+                if(e.X>(s.GetX()) && e.X < (s.GetX() + s.GetSize() ) 
+                    && e.Y > (s.GetY()) && e.Y < (s.GetY()+ s.GetSize()))
+                {
+                    if(activeShape != i)
+                    {
+                        activeShape = i;
+                        change = true;
+                        break;
+                    }
+                    
+                }
+                i++;
+            }
+            if (!change)
+            {
+                shapes[activeShape].SetPos(e.X, e.Y);
+                drawShape();
+            }
+        }
+        
+    }
+
+    public class Shape
+    {
+        int posX;
+        int posY;
+        int size;
+        SolidBrush b;
+        char type;
+
+        public Shape(int X, int Y, int _size, Color c,char _type)
+        {
+            posX = X;
+            posY = Y;
+            size = _size;
+            b = new SolidBrush(c);
+            type = _type;
+        }
+
+        public int GetX()
+        {
+            return posX;
+        }
+
+        public int GetY()
+        {
+            return posY;
+        }
+
+        public int GetSize()
+        {
+            return size;
+        }
+
+        public SolidBrush GetBrush()
+        {
+            return b;
+        }
+
+        public char GetType()
+        {
+            return type;
+        }
+
+        public void SetPos(int _x, int _y)
+        {
+            posX = _x - size / 2;
+            posY = _y - size / 2;
+        }
+
+        public void SetSize(int _s)
+        {
+            size = _s ;
+        }
+
+        public void SetBrush(Color _c)
+        {
+            b = new SolidBrush(_c);
+        }
+    }
+}
